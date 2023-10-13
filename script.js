4ac87b8127dde1b5db21ff432a4947748b146c3d
edgar-karapetyan <147828488+edgar-karapetyan@users.noreply.github.com>
2023-10-13 17:04:23 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,46 @@
-document.querySelector('.add_btn').addEventListener('click', () => {
-    let input_todo = document.querySelector('.input_todo'),
-        todos_container = document.querySelector('.todos');
-
-    if (input_todo.value !== '') {
-        todos_container.innerHTML += `
-        <li class="new_todos">
-            <input type="checkbox" id="checkbox">
-            <span class="texts">${input_todo.value}</span>
-            <button class="redact"><img src="./img/edit.png" alt="trash"></button>
-            <button class="delet"><img src="./img/trash-red.svg" alt="trash"></button>
-        </li>`;
-
-        let delet = document.querySelectorAll('.delet');
-        document.querySelector('.todo_list_count').textContent = `You have ${document.querySelectorAll('.new_todos').length} pending tasks`
-        for (let i = 0; i < delet.length; i++) {
-            let clear_all = document.querySelector('.clear_all'),
-                checkbox = document.querySelectorAll('#checkbox');
-
-            delet[i].addEventListener('click', () => {
-                let new_todos = document.querySelectorAll('.new_todos');
-                delet[i].parentNode.remove(new_todos[i])
-                document.querySelector('.todo_list_count').textContent = `You have ${new_todos.length - 1} pending tasks`
-            })
-            clear_all.addEventListener('click', () => {
-                let new_todos = document.querySelectorAll('.new_todos');
-                new_todos[i].remove()
-                document.querySelector('.todo_list_count').textContent = `You have 0 pending tasks`
-            })
-            checkbox[i].addEventListener('click', () => {
-                texts = document.querySelectorAll('.texts');
-                if (checkbox[i].checked) {
-                    texts[i].style.cssText = 'text-decoration-line: line-through;'
-                    checkbox[i].style.cssText = 'filter: saturate(1);'
-                } else {
-                    texts[i].style.cssText = 'text-decoration-line: unset;'
-                    checkbox[i].style.cssText = 'filter: saturate(0);'
-                }
-            })
-        }
-
-        input_todo.value = ''
-        document.querySelector('.create_new_todo').classList.remove('enterTextPopuo');
+// card-ui-hover/vanilla-tilt - setings
+VanillaTilt.init(document.querySelectorAll(".vanilla-tilt-3D"), {
+    max: 5,
+    speed: 400,
+    scale: 1.1
+});
+// serch placeholder from all brousers
+document.querySelector('.inp-custom-placeholder').addEventListener('input', (e) => {
+    if (e.target.value) {
+        document.querySelector('.custom-placeholder').classList.add('hide')
     } else {
-        document.querySelector('.create_new_todo').classList.add('enterTextPopuo');
-    }
-
-    // ______________________________redact logic______________________________
-   let redact_btn = document.querySelectorAll('.redact'),
-    texts = document.querySelectorAll('.texts');
-    for (let i = 0; i < redact_btn.length; i++) {
-        redact_btn[i].addEventListener('click', () => {
-            if (texts[i].contentEditable == "true") {
-                texts[i].contentEditable = "false";
-                redact_btn[i].classList.remove('redact_Befor')
-            } else {
-                texts[i].contentEditable = "true";
-                redact_btn[i].classList.add('redact_Befor')
-            }
-        })
+        document.querySelector('.custom-placeholder').classList.remove('hide')
     }
 })
+// nav right corner open close
+document.querySelector('.nav-right-corner-btn-open').addEventListener('click', () => {
+    if (document.querySelector('.nav-right-corner').style.right == '0px') {
+        document.querySelector('.nav-right-corner').style.right = '-37px'
+        document.querySelector('.nav-right-corner-btn-open .arrow').style.cssText = 'transform: rotate(0deg);'
+    } else {
+        document.querySelector('.nav-right-corner').style.right = '0px'
+        document.querySelector('.nav-right-corner-btn-open .arrow').style.cssText = 'transform: rotate(180deg);'
+    }
+
+})
+// mobile panel open close
+const nav_items_cont_mob = document.querySelector('.nav-items-cont-mob')
+document.querySelector('.nav-items-cont-mob-close').addEventListener('click', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 0.5; top: -80vh;'
+})
+// mobile panel serch animation
+const search_mob = document.querySelector('.search-mob')
+search_mob.addEventListener('click', () => {
+    search_mob.style.width = '160px'
+})
+document.querySelector('.carousel').addEventListener('click', () => { search_mob.style.width = '30px' })
+document.querySelector('.nav-items-cont-mob-open').addEventListener('click', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 1; top: 0;'
+})
+// scrill close
+window.addEventListener('scroll', () => {
+    nav_items_cont_mob.style.cssText = 'opacity: 0.5; top: -80vh;'
+    search_mob.style.width = '30px'
+    search_mob.querySelector('.inp').style.cssText = 'opacity: 0;'
+})
+// random blobs animaation
